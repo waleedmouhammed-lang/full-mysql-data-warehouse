20d83385d901c645cbf057b8bdc18d08b34281dd
Waleed Mouhammed <waleed.mouhammed@icloud.com>
2025-11-16 07:25:35 +0200

    Modified DDL through loading the data using VARCHAR(255) data type for all columns regardless their original data type as it is the professional best practice for the bronze layer

--- dw_creation_script.sql
+++ dw_creation_script.sql
@@ -29,64 +29,65 @@ CREATE DATABASE IF NOT EXISTS dw_gold;
 -- SQL Server you need to build T-SQL logic to check the existence of the table first.
 -- If it does exist, then drop it, otherwise create it directly.
 DROP TABLE IF EXISTS dw_bronze.crm_cust_info;
+-- Creating tables schema using VARCHAR(255) ensure proper raw data loading without any data loss
 CREATE TABLE IF NOT EXISTS dw_bronze.crm_cust_info(
-    cst_id INT,
-    cst_key VARCHAR(50),
-    cst_firstname VARCHAR(50),
-    cst_lastname VARCHAR(50),
-    cst_marital_status VARCHAR(50),
-    cst_gndr VARCHAR(50),
-    cst_create_date DATETIME
+    cst_id VARCHAR(255),
+    cst_key VARCHAR(255),
+    cst_firstname VARCHAR(255),
+    cst_lastname VARCHAR(255),
+    cst_marital_status VARCHAR(255),
+    cst_gndr VARCHAR(255),
+    cst_create_date VARCHAR(255)
 );
 
 -- Create product info table
 DROP TABLE IF EXISTS dw_bronze.crm_prd_info;
 CREATE TABLE IF NOT EXISTS dw_bronze.crm_prd_info(
-    prd_id INT,
-    prd_key VARCHAR(50),
-    prd_nm VARCHAR(50),
-    prd_cost DECIMAL,
-    prd_line VARCHAR(10),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME
+    prd_id VARCHAR(255),
+    prd_key VARCHAR(255),
+    prd_nm VARCHAR(255),
+    prd_cost VARCHAR(255),
+    prd_line VARCHAR(255),
+    prd_start_dt VARCHAR(255),
+    prd_end_dt VARCHAR(255)
 );
 
 -- Creating sales details table
 DROP TABLE IF EXISTS dw_bronze.crm_sales_details;
 CREATE TABLE IF NOT EXISTS dw_bronze.crm_sales_details(
-    sls_ord_num VARCHAR(50),
-    sls_prd_key VARCHAR(50),
-    sls_cust_id INT,
-    sls_order_dt DATE,
-    sls_ship_dt DATE,
-    sls_due_dt DATE,
-    sls_sales INT,
-    sls_quantity INT,
-    sls_price INT
+    sls_ord_num VARCHAR(255),
+    sls_prd_key VARCHAR(255),
+    sls_cust_id VARCHAR(255),
+    sls_order_dt VARCHAR(255),
+    sls_ship_dt VARCHAR(255),
+    sls_due_dt VARCHAR(255),
+    sls_sales VARCHAR(255),
+    sls_quantity VARCHAR(255),
+    sls_price VARCHAR(255)
 );
 
 -- Creating erp_cust_az12 table logic
 DROP TABLE IF EXISTS dw_bronze.erp_cust_az12;
 CREATE TABLE IF NOT EXISTS dw_bronze.erp_cust_az12(
-    CID VARCHAR(50),
-    BDATE DATETIME,
-    GEN VARCHAR(50)
+    CID VARCHAR(255),
+    BDATE VARCHAR(255),
+    GEN VARCHAR(255)
 );
 
 -- Creating erp_loc_a101 table logic
 DROP TABLE IF EXISTS dw_bronze.erp_loc_a101;
 CREATE TABLE IF NOT EXISTS dw_bronze.erp_loc_a101(
-    CID VARCHAR(50),
-    CNTRY VARCHAR(50)
+    CID VARCHAR(255),
+    CNTRY VARCHAR(255)
 );
 
 -- Creating erp_px_cat_g1v2 table logic
 DROP TABLE IF EXISTS dw_bronze.erp_px_cat_g1v2;
 CREATE TABLE IF NOT EXISTS dw_bronze.erp_px_cat_g1v2(
-    ID VARCHAR(50),
-    CAT VARCHAR(50),
-    SUBCAT VARCHAR(50),
-    MAINTENANCE VARCHAR(10)
+    ID VARCHAR(255),
+    CAT VARCHAR(255),
+    SUBCAT VARCHAR(255),
+    MAINTENANCE VARCHAR(255)
 );
 
 /*
@@ -95,13 +96,16 @@ CREATE TABLE IF NOT EXISTS dw_bronze.erp_px_cat_g1v2(
     - It's called data loading infile
 */
 
--- Setting the local infile loading
+-- Setting the local infile loading to enable local file loading into the database
 SET GLOBAL local_infile = 1;
 
+-- Show the variable status to ensure that the local file loading is enabled
 -- SHOW GLOBAL VARIABLES LIKE 'local_infile';
+
+-- Identifying the secured place to store the csv files in to ease the loading process
 -- SHOW VARIABLES LIKE 'secure_file_priv';
 
--- Load the first raw data set into the first table
+-- Starting the raw data loading process
 TRUNCATE TABLE dw_bronze.crm_cust_info;
 
 LOAD DATA LOCAL INFILE '/Users/waleedmouhammed/Documents/GitHub/dw_dev/datasets/source_crm/cust_info.csv'
@@ -131,17 +135,18 @@ OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-select * from dw_bronze.crm_cust_info;
-select * from dw_bronze.crm_prd_info;
-select * from dw_bronze.crm_sales_details;
-
+-- Truncating the table to ensure full refresh load
 TRUNCATE TABLE dw_bronze.erp_cust_az12;
 
+-- Using the LOCAL keyword to load data from local file system
 LOAD DATA LOCAL INFILE '/Users/waleedmouhammed/Documents/GitHub/dw_dev/datasets/source_erp/cust_az12.csv'
+-- Using the schema name as a prefix to ensure proper script execution
 INTO TABLE dw_bronze.erp_cust_az12
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
+-- Adding \r\n for Windows line endings
 LINES TERMINATED BY '\r\n'
+-- Ignoring one line as we have column headers in the first line
 IGNORE 1 LINES;
 
 TRUNCATE TABLE dw_bronze.erp_loc_a101;
@@ -162,6 +167,10 @@ OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-select * from dw_bronze.erp_cust_az12;
-select * from dw_bronze.erp_loc_a101;
-select * from dw_bronze.erp_px_cat_g1v2;
+-- Validating the full data loading into the bronze layer - from quantity perspective!
+select COUNT(*) from dw_bronze.crm_cust_info;
+select COUNT(*) from dw_bronze.crm_prd_info;
+select COUNT(*) from dw_bronze.crm_sales_details;
+select COUNT(*) from dw_bronze.erp_cust_az12;
+select COUNT(*) from dw_bronze.erp_loc_a101;
+select COUNT(*) from dw_bronze.erp_px_cat_g1v2;
